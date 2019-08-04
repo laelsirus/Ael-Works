c3754bc747711ca3c8136b434428420856e652a2
Ael <laelsirus@gmail.com>
2019-08-04 23:39:36 +0900

    add expander

---
+++ js/expander.js
@@ -0,0 +1,63 @@
+function sidebar_toggle_fluid() {
+  var root = document.documentElement;
+  var buttonText = document.getElementById('FSFluid');
+  if (~root.className.indexOf(' selector')) {
+    root.className = root.className.replace(' selector', '');
+    $(document.getElementById("ninja-slider1")).css("-webkit-transform","scale(1,1)");
+    $(document.getElementById("ninja-slider1")).css("background-color","rgba(0,0,0,0)");
+    buttonText.value = "확대";
+  } else {
+    root.className += ' selector';
+    $(document.getElementById("ninja-slider1")).css("-webkit-transform","scale(1.3,1.3)");
+    $(document.getElementById("ninja-slider1")).css("background-color","rgba(0,0,0,0.7");
+    buttonText.value = "축소";
+  };
+};
+
+function sidebar_toggle_dongju() {
+  var root = document.documentElement;
+  var buttonText = document.getElementById('FSDongju');
+  if (~root.className.indexOf(' selector')) {
+    root.className = root.className.replace(' selector', '');
+    $(document.getElementById("ninja-slider2")).css("-webkit-transform","scale(1,1)");
+    $(document.getElementById("ninja-slider2")).css("background-color","rgba(0,0,0,0)");
+    buttonText.value = "확대";
+  } else {
+    root.className += ' selector';
+    $(document.getElementById("ninja-slider2")).css("-webkit-transform","scale(1.3,1.3)");
+    $(document.getElementById("ninja-slider2")).css("background-color","rgba(0,0,0,0.7");
+    buttonText.value = "축소";
+  };
+};
+
+function sidebar_toggle_trace() {
+  var root = document.documentElement;
+  var buttonText = document.getElementById('FSTrace');
+  if (~root.className.indexOf(' selector')) {
+    root.className = root.className.replace(' selector', '');
+    $(document.getElementById("ninja-slider3")).css("-webkit-transform","scale(1,1)");
+    $(document.getElementById("ninja-slider3")).css("background-color","rgba(0,0,0,0)");
+    buttonText.value = "확대";
+  } else {
+    root.className += ' selector';
+    $(document.getElementById("ninja-slider3")).css("-webkit-transform","scale(1.3,1.3)");
+    $(document.getElementById("ninja-slider3")).css("background-color","rgba(0,0,0,0.7");
+    buttonText.value = "축소";
+  };
+};
+
+function sidebar_toggle_sharp() {
+  var root = document.documentElement;
+  var buttonText = document.getElementById('FSSharp');
+  if (~root.className.indexOf(' selector')) {
+    root.className = root.className.replace(' selector', '');
+    $(document.getElementById("ninja-slider4")).css("-webkit-transform","scale(1,1)");
+    $(document.getElementById("ninja-slider4")).css("background-color","rgba(0,0,0,0)");
+    buttonText.value = "확대";
+  } else {
+    root.className += ' selector';
+    $(document.getElementById("ninja-slider4")).css("-webkit-transform","scale(1.3,1.3)");
+    $(document.getElementById("ninja-slider4")).css("background-color","rgba(0,0,0,0.7");
+    buttonText.value = "축소";
+  };
+};
