9bb1ecc699a4da8536fd2018f7f81ecbc61e34ab
Ael <laelsirus@gmail.com>
2019-08-08 15:22:07 +0900

    add expander overflow

--- js/expander.js
+++ js/expander.js
@@ -5,12 +5,14 @@ function sidebar_toggle_fluid() {
     root.className = root.className.replace(' selector', '');
     $(document.getElementById("ninja-slider1")).css("-webkit-transform","scale(1,1)");
     $(document.getElementById("ninja-slider1")).css("background-color","rgba(0,0,0,0)");
+    $(document.getElementsByTagName("body")).css("overflow","auto");
     $(document.getElementById("ninja-slider1")).toggleClass("emphasized");
     buttonText.value = "EXPAND";
   } else {
     root.className += ' selector';
     $(document.getElementById("ninja-slider1")).css("-webkit-transform","scale(1.2,1.2)");
     $(document.getElementById("ninja-slider1")).css("background-color","rgba(0,0,0,0.7");
+    $(document.getElementsByTagName("body")).css("overflow","hidden");
     $(document.getElementById("ninja-slider1")).toggleClass("emphasized");
     buttonText.value = "CLOSE";
 
@@ -32,12 +34,14 @@ function sidebar_toggle_dongju() {
     root.className = root.className.replace(' selector', '');
     $(document.getElementById("ninja-slider2")).css("-webkit-transform","scale(1,1)");
     $(document.getElementById("ninja-slider2")).css("background-color","rgba(0,0,0,0)");
+    $(document.getElementsByTagName("body")).css("overflow","auto");
     $(document.getElementById("ninja-slider2")).toggleClass("emphasized");
     buttonText.value = "EXPAND";
   } else {
     root.className += ' selector';
     $(document.getElementById("ninja-slider2")).css("-webkit-transform","scale(1.2,1.2)");
     $(document.getElementById("ninja-slider2")).css("background-color","rgba(0,0,0,0.7");
+    $(document.getElementsByTagName("body")).css("overflow","hidden");
     $(document.getElementById("ninja-slider2")).toggleClass("emphasized");
     buttonText.value = "CLOSE";
   };
@@ -51,12 +55,14 @@ function sidebar_toggle_trace() {
     root.className = root.className.replace(' selector', '');
     $(document.getElementById("ninja-slider3")).css("-webkit-transform","scale(1,1)");
     $(document.getElementById("ninja-slider3")).css("background-color","rgba(0,0,0,0)");
+    $(document.getElementsByTagName("body")).css("overflow","auto");
     $(document.getElementById("ninja-slider3")).toggleClass("emphasized");
     buttonText.value = "EXPAND";
   } else {
     root.className += ' selector';
     $(document.getElementById("ninja-slider3")).css("-webkit-transform","scale(1.2,1.2)");
     $(document.getElementById("ninja-slider3")).css("background-color","rgba(0,0,0,0.7");
+    $(document.getElementsByTagName("body")).css("overflow","hidden");
     $(document.getElementById("ninja-slider3")).toggleClass("emphasized");
     buttonText.value = "CLOSE";
   };
@@ -69,12 +75,14 @@ function sidebar_toggle_sharp() {
     root.className = root.className.replace(' selector', '');
     $(document.getElementById("ninja-slider4")).css("-webkit-transform","scale(1,1)");
     $(document.getElementById("ninja-slider4")).css("background-color","rgba(0,0,0,0)");
+    $(document.getElementsByTagName("body")).css("overflow","auto");
     $(document.getElementById("ninja-slider4")).toggleClass("emphasized");
     buttonText.value = "EXPAND";
   } else {
     root.className += ' selector';
     $(document.getElementById("ninja-slider4")).css("-webkit-transform","scale(1.2,1.2)");
     $(document.getElementById("ninja-slider4")).css("background-color","rgba(0,0,0,0.7");
+    $(document.getElementsByTagName("body")).css("overflow","hidden");
     $(document.getElementById("ninja-slider4")).toggleClass("emphasized");
     buttonText.value = "CLOSE";
   };
@@ -88,12 +96,14 @@ function sidebar_toggle_sound() {
     root.className = root.className.replace(' selector', '');
     $(document.getElementById("ninja-slider5")).css("-webkit-transform","scale(1,1)");
     $(document.getElementById("ninja-slider5")).css("background-color","rgba(0,0,0,0)");
+    $(document.getElementsByTagName("body")).css("overflow","auto");
     $(document.getElementById("ninja-slider5")).toggleClass("emphasized");
     buttonText.value = "EXPAND";
   } else {
     root.className += ' selector';
     $(document.getElementById("ninja-slider5")).css("-webkit-transform","scale(1.2,1.2)");
     $(document.getElementById("ninja-slider5")).css("background-color","rgba(0,0,0,0.7");
+    $(document.getElementsByTagName("body")).css("overflow","hidden");
     $(document.getElementById("ninja-slider5")).toggleClass("emphasized");
     buttonText.value = "CLOSE";
   };
